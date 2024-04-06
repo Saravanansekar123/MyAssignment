9ff0d42dbc4474b8bc82cd86a378d51c5931fe46
Saravanansekar123 <166205314+Saravanansekar123@users.noreply.github.com>
2024-04-06 12:48:25 +0530

    Day1

---
+++ day1/Firefox.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Firefox {
+	
+	public static void main(String[] args) {
+		float browserVersion =100.2f;
+		String browserName ="firefox";
+		int ReleaseYear=2024;
+		char BrowserLogo ='f';
+		boolean isVariable=true;
+		System.out.println (browserVersion+"\n"+browserName+"\n"+ReleaseYear+"\n"+BrowserLogo+"\n"+isVariable);
+	}
+
+}
